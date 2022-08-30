d98b08a29b565c3b1d93537a157a9080cec25529
wilsonjpdev <wilsonjp.dev@outlook.com>
2022-08-30 11:33:49 -0300

    Relacionamentos iniciais

---
+++ app/Models/Store.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
+}
